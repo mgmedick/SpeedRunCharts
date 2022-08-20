b1897693a3ba2ce4b95ab0ed297d67e143233d93
Michael Medick <mgmedick@live.com>
2022-08-20 13:46:04 -0500

    Fixing column size of summary for mobile.

--- SpeedRunApp.UI/src/components/SpeedRunSummaryVue.vue
+++ SpeedRunApp.UI/src/components/SpeedRunSummaryVue.vue
@@ -22,7 +22,7 @@
                 </div>
             </div>
             <div class="container px-2 pb-2 pt-1 d-flex">
-                <div class="col-9 p-0 align-self-center">
+                <div class="col-sm-9 p-0 align-self-center">
                      <div>   
                         <div class="text-secondary nowrap-elipsis" style="font-size: 14px;">
                             <a :href="'/Game/GameDetails/' + item.game.abbr + '?speedRunID=' + item.speedRunComID" class="text-primary"><template v-if="item.rankString"><i v-if="getIconClass(item.rank)" class="fa fa-trophy pr-1" :class="getIconClass(item.rank)"></i><span>{{ item.rankString }}</span>&nbsp;-&nbsp;</template><small>{{ item.primaryTimeString }}</small></a>
